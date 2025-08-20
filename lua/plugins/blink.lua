d52234a409b340938c39ecb29b9f9d44600c9731
minco <mail@drchi.co.kr>
2025-08-21 01:03:22 +0900

    feat: comment

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,36 +1,36 @@
 return {
-  'saghen/blink.nvim',
-  build = 'cargo build --release', -- for delimiters
-  keys = {
-	-- chartoggle
-	{
-	  '<C-;>',
-	  function()
-	  	require('blink.chartoggle').toggle_char_eol(';')
-	  end,
-	  mode = { 'n', 'v' },
-	  desc = 'Toggle ; at eol',
-	},
-	{
-	  ',',
-	  function()
-	  	require('blink.chartoggle').toggle_char_eol(',')
-	  end,
-	  mode = { 'n', 'v' },
-	  desc = 'Toggle , at eol',
-	},
+    'saghen/blink.nvim',
+    build = 'cargo build --release', -- for delimiters
+    keys = {
+        -- chartoggle
+        {
+            '<C-;>',
+            function()
+                require('blink.chartoggle').toggle_char_eol(';')
+            end,
+            mode = { 'n', 'v' },
+            desc = 'Toggle ; at eol',
+        },
+        {
+            ',',
+            function()
+                require('blink.chartoggle').toggle_char_eol(',')
+            end,
+            mode = { 'n', 'v' },
+            desc = 'Toggle , at eol',
+        },
 
-	-- tree
-	{ '<C-e>', '<cmd>BlinkTree reveal<cr>', desc = 'Reveal current file in tree' },
-	{ '<leader>E', '<cmd>BlinkTree toggle<cr>', desc = 'Reveal current file in tree' },
-	{ '<leader>e', '<cmd>BlinkTree toggle-focus<cr>', desc = 'Toggle file tree focus' },
-  },
-  -- all modules handle lazy loading internally
-  lazy = false,
-  opts = {
-    chartoggle = { enabled = true },
-    tree = { enabled = true },
-    cmp = { enabled = true },
-    pairs = { enabled = true },
-  }
+        -- tree
+        -- { '<C-e>', '<cmd>BlinkTree reveal<cr>', desc = 'Reveal current file in tree' },
+        -- { '<leader>E', '<cmd>BlinkTree toggle<cr>', desc = 'Reveal current file in tree' },
+        -- { '<leader>e', '<cmd>BlinkTree toggle-focus<cr>', desc = 'Toggle file tree focus' },
+    },
+    -- all modules handle lazy loading internally
+    lazy = false,
+    opts = {
+        chartoggle = { enabled = true },
+        tree = { enabled = false },
+        cmp = { enabled = true },
+        pairs = { enabled = true },
+    }
 }
