e4afa6b7072f0bdff85e55f9ae675439caae54cc
minco <mail@drchi.co.kr>
2025-11-25 13:48:39 +0900

    add hpack

--- lua/config/hpack.lua
+++ lua/config/hpack.lua
@@ -1,6 +1,5 @@
 local function hpack()
-    local file = vim.fn.expand("%")
-    local err = vim.fn.system("hpack " .. file)
+    local err = vim.fn.system("hpack")
     if vim.v.shell_error ~= 0 then
         print(err)
     end
@@ -8,5 +7,10 @@ end
 
 vim.api.nvim_create_autocmd("BufWritePost", {
     pattern = "package.yaml",
+    callback = hpack,
+})
+
+vim.api.nvim_create_autocmd("BufEnter", {
+    pattern = "*.hs",
     callback = hpack,
 })
--- lua/plugins/haskell-mod.lua
+++ lua/plugins/haskell-mod.lua
@@ -1,4 +1,12 @@
-return {
+local prod = {
     "mincomk/haskell-mod.nvim",
     config = true
 }
+
+local dev = {
+    dir = "~/projects/haskell-mod.nvim/",
+    name = "haskell-mod.nvim",
+    config = true
+}
+
+return prod
