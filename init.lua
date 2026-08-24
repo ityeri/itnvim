b5276a14089ef94f16fbdac6129158ed52a12b8e
ityeri <ityeri9@gmail.com>
2026-08-24 13:39:04 +0900

    add: Base keymapping settings

---
+++ init.lua
@@ -0,0 +1,13 @@
+vim.opt.clipboard = "unnamedplus"
+vim.g.mapleader = " "
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
