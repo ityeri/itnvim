60ac75957039e7c89fcc7928ceaf29db98bf373b
ityeri <ityeri9@gmail.com>
2026-08-25 22:01:43 +0900

    add: lazydev plugin

--- lua/config/lazy_boot.lua
+++ lua/config/lazy_boot.lua
@@ -6,8 +6,8 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
   if vim.v.shell_error ~= 0 then
     vim.api.nvim_echo({
       { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
-      { "\nPress any key to exit..." },
+      { out,                            "WarningMsg" },
+      { "\nPress any key to exit..." }
     }, true, {})
     vim.fn.getchar()
     os.exit(1)
@@ -25,12 +25,11 @@ vim.g.maplocalleader = "\\"
 require("lazy").setup({
   spec = {
     -- import your plugins
-    { import = "plugins" },
+    { import = "plugins" }
   },
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
   -- install = { colorscheme = { "habamax" } },
   -- automatically check for plugin updates
-  checker = { enabled = true },
+  checker = { enabled = true }
 })
-
