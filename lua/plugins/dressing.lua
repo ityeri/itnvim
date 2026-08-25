d992c8d9625a1055a7867a1750c810f8f0fab5f9
ityeri <ityeri9@gmail.com>
2026-08-26 00:06:16 +0900

    add: Dressing plugin

---
+++ lua/plugins/dressing.lua
@@ -0,0 +1,13 @@
+return {
+  "stevearc/dressing.nvim",
+  opts = {
+    input = {
+      start_mode = "normal",
+      float = {
+        relative = "cursor",
+        row = 0,
+        col = 0
+      }
+    }
+  }
+}
