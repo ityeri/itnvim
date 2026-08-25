a3c666b25df4b5515dac6eec5c2de0aeccb04fc3
ityeri <ityeri9@gmail.com>
2026-08-25 22:52:47 +0900

    add: Display error or warning message via virtual text

--- init.lua
+++ init.lua
@@ -1,19 +1,37 @@
-vim.opt.clipboard = "unnamedplus"
-vim.g.mapleader = " "
-vim.opt.number = true
-vim.opt.relativenumber = true
+do -- General nvim native settings
+  vim.opt.clipboard = "unnamedplus"
+  vim.g.mapleader = " "
 
-vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
-vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
-vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
-vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
+  vim.opt.number = true
+  vim.opt.relativenumber = true
 
-vim.keymap.set("n", "-", "^", { desc = "Goto start of the line" })
+  vim.diagnostic.config({ virtual_text = true })
 
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 2
-vim.opt.expandtab = true
-vim.opt.statuscolumn = " %@v:lua.require'snacks.statuscolumn'@%s%=%l │"
+  vim.opt.tabstop = 4
+  vim.opt.shiftwidth = 2
+  vim.opt.expandtab = true
+  vim.opt.statuscolumn = " %@v:lua.require'snacks.statuscolumn'@%s%=%l │"
 
-require("config.lazy_boot") -- Bootstrapping lazynvim
-require("config.lsp")
+  -- Error & Warning message displaying
+  vim.diagnostic.config({
+    underline = true,
+    virtual_text = true,
+    signs = true,
+    update_in_insert = false,
+    severity_sort = true
+  })
+end
+
+do -- Native feature keymap settings
+  vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
+  vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
+  vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
+  vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
+
+  vim.keymap.set("n", "-", "^", { desc = "Goto start of the line" })
+end
+
+do -- Load extrenal config, modules
+  require("config.lazy_boot")
+  require("config.lsp")
+end
