5d9234b827942bb626b28ecdba05853db4dc326a
ityeri <ityeri9@gmail.com>
2026-08-25 22:34:13 +0900

    add: lazydev.lua: build setting

--- lua/plugins/lazydev.lua
+++ lua/plugins/lazydev.lua
@@ -23,6 +23,11 @@ return {
   { -- optional blink completion source for require statements and module annotations
     "saghen/blink.cmp",
     version = "2.*",
+    build = function()
+      -- build the fuzzy matcher, optionally add a timeout to `pwait(timeout_ms)`
+      -- you can use `gb` in `:Lazy` to rebuild the plugin as needed
+      require("blink.cmp").build():pwait()
+    end,
     dependencies = {
       "saghen/blink.lib"
     },
