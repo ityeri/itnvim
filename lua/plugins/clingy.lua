dca9d7b9efc2bb43177075c30fe6e9acd7b37bed
ityeri <ityeri9@gmail.com>
2026-08-25 19:53:33 +0900

    format: Indent in clingy.lua

--- lua/plugins/clingy.lua
+++ lua/plugins/clingy.lua
@@ -1,12 +1,12 @@
 return {
   {
-        "ityeri/clingy.nvim",
-        opts = {
-          enabled = true,       -- 기본이 false라 시작 시 켜려면 필수
-          relative_nr = true,   -- 상대 줄번호 (기본값)
-          skip_empty_lines = false,
-          threshold = 0,        -- 번호와 텍스트 시작점의 간격(열)
-        },
-      },
+    "ityeri/clingy.nvim",
+    opts = {
+      enabled = true,
+      relative_nr = true,
+      skip_empty_lines = false,
+      threshold = 0
+    }
+  }
 }
 
