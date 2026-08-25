c5cb29281e09b5c6e92daf49c832ce39d4433ab9
ityeri <ityeri9@gmail.com>
2026-08-25 20:15:45 +0900

    add: Mason ensure_installed setting

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,5 +1,18 @@
 return {
-  "mason-org/mason.nvim",
-  opts = {}
+  {
+    "mason-org/mason.nvim",
+    opts = {}
+  },
+  {
+    "mason-org/mason-lspconfig.nvim",
+    opts = {
+      ensure_installed = {
+        -- These names are from the package name. See the next of the lsp name in mason setting view
+        "lua_ls",
+        "rust_analyzer"
+      },
+      automatic_enable = true
+    },
+    dependencies = { "neovim/nvim-lspconfig" }
+  }
 }
-
