4d19d1082997d2ea23e696044968ed00036bc979
ityeri <ityeri9@gmail.com>
2026-08-25 20:29:34 +0900

    add: Telescope plugin

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,41 @@
+return {
+  "nvim-telescope/telescope.nvim",
+  version = "*",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    -- optional but recommended
+    { "nvim-telescope/telescope-fzf-native.nvim", build = "make" }
+  },
+  config = function()
+    local actions = require "telescope.actions"
+    require("telescope").setup {
+      defaults = {
+        mappings = {
+          i = {
+            ["<C-j>"] = actions.move_selection_next,
+            ["<C-k>"] = actions.move_selection_previous
+          }
+        }
+      },
+      pickers = {
+        -- Default configuration for builtin pickers goes here:
+        -- picker_name = {
+        --   picker_config_key = value,
+        --   ...
+        -- }
+        -- Now the picker_config_key will be applied every time you call this
+        -- builtin picker
+      },
+      extensions = {
+        -- Your extension configuration goes here:
+        -- extension_name = {
+        --   extension_config_key = value,
+        -- }
+        -- please take a look at the readme of the extension you want to configure
+      }
+    }
+
+    local builtin = require("telescope.builtin")
+    vim.keymap.set("n", "<C-f>", builtin.live_grep)
+  end
+}
