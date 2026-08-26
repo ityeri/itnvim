7e532dc0a00cf63178be9a1b6f580aa54d7d5335
ityeri <ityeri9@gmail.com>
2026-08-26 10:14:22 +0900

    rm: Duplicated plugin setting: nvim-cmp, blink-cmp has removed from lazydev.lua

--- lua/plugins/lazydev.lua
+++ lua/plugins/lazydev.lua
@@ -9,42 +9,6 @@ return {
         { path = "${3rd}/luv/library", words = { "vim%.uv" } }
       }
     }
-  },
-  { -- optional cmp completion source for require statements and module annotations
-    "hrsh7th/nvim-cmp",
-    opts = function(_, opts)
-      opts.sources = opts.sources or {}
-      table.insert(opts.sources, {
-        name = "lazydev",
-        group_index = 0 -- set group index to 0 to skip loading LuaLS completions
-      })
-    end
-  },
-  { -- optional blink completion source for require statements and module annotations
-    "saghen/blink.cmp",
-    version = "2.*",
-    build = function()
-      -- build the fuzzy matcher, optionally add a timeout to `pwait(timeout_ms)`
-      -- you can use `gb` in `:Lazy` to rebuild the plugin as needed
-      require("blink.cmp").build():pwait()
-    end,
-    dependencies = {
-      "saghen/blink.lib"
-    },
-    opts = {
-      sources = {
-        -- add lazydev to your completion providers
-        default = { "lazydev", "lsp", "path", "snippets", "buffer" },
-        providers = {
-          lazydev = {
-            name = "LazyDev",
-            module = "lazydev.integrations.blink",
-            -- make lazydev completions top priority (see `:h blink.cmp`)
-            score_offset = 100
-          }
-        }
-      }
-    }
   }
   -- { "folke/neodev.nvim", enabled = false }, -- make sure to uninstall or disable neodev.nvim
 }
